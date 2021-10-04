978fc58cf79430091e9bf0f880b2a71407632f00
TopModeJapan <lolikiller157@gmail.com>
2021-10-04 21:58:58 +0700

    Lu Semua Anjing Makasih

---
+++ routes/main.js
@@ -0,0 +1,10 @@
+__path = process.cwd()
+
+var express = require('express');
+var router = express.Router();
+
+router.get('/', (req, res) => {
+    res.sendFile(__path + '/views/home.html')
+})
+
+module.exports = router
